2b051b5a509f60cf8cfcafcbf14ae1a5be515b17
Jesse <jvanderdoes@gmail.com>
2016-08-03 20:51:42 -0700

    Destroy the ball after 5 seconds

---
+++ Assets/Autodestruct.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Autodestruct : MonoBehaviour {
+
+    int createdAt;
+
+    public int destroyAfter = 5;
+
+	// Use this for initialization
+	void Start () {
+        TimeSpan t = DateTime.UtcNow - new DateTime(1970, 1, 1);
+        createdAt = (int)t.TotalSeconds;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        TimeSpan t = DateTime.UtcNow - new DateTime(1970, 1, 1);
+        int now = (int)t.TotalSeconds;
+        if ((now - createdAt) > destroyAfter)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
