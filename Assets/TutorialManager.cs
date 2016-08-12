7fb68d9eb37b67ac93396b166a7b59db7491c8ba
Bryan <Bryan@DESKTOP-9J1E7LU>
2016-08-12 09:34:14 -0700

    progress on tutorial sounds

--- Assets/TutorialManager.cs
+++ Assets/TutorialManager.cs
@@ -2,16 +2,18 @@
 using System.Collections;
 
 public class TutorialManager : MonoBehaviour {
-    private AudioClip Blip;
-    private AudioClip Drop;
-    private AudioSource audio;
+    public AudioClip startTutorialSound;
+    public AudioClip startBlockTutorial;
+    public AudioClip endBlockTutorial;
+    public AudioClip logicBlockTutorial;
+    private new AudioSource audio;
     private bool placedStartBlock = false;
     private bool placedEndBlock = false;
 
 	// Use this for initialization
 	void Start () {
-        audio = GetComponent<AudioSource>();
-        Invoke("StartTutorial", 5);
+       audio = GetComponent<AudioSource>();
+       Invoke("StartTutorial", 5);
 	}
 	
 	// Update is called once per frame
@@ -21,23 +23,31 @@ public class TutorialManager : MonoBehaviour {
 
     void StartTutorial() {
         Debug.Log("second llog");
-        audio.PlayOneShot(Drop);
+        audio.PlayOneShot(startTutorialSound);
+
+
+
     }
 
     void PlacedStartBlock()
     {
-        
         if(placedStartBlock == false)
         {
-          
+            /* Play End Block tutorial */
+            audio.PlayOneShot(startBlockTutorial);
+
+
             placedStartBlock = true;
         }
     }
 
     void PlacedEndBlock()
     {
-        if(placedEndBlock == false)
+        if (placedEndBlock == false)
         {
+            /* Play Logic Block tutorial */
+            audio.PlayOneShot(logicBlockTutorial);
+
             Debug.Log("Placed the end block");
             placedEndBlock = true;
         }   
