d8505b254c58b4180e99180ca49f34f83d9435d6
Bryan <Bryan@DESKTOP-9J1E7LU>
2016-08-12 13:27:31 -0700

    instructions are playing

--- Assets/TutorialManager.cs
+++ Assets/TutorialManager.cs
@@ -2,13 +2,22 @@
 using System.Collections;
 
 public class TutorialManager : MonoBehaviour {
-    public AudioClip startTutorialSound;
-    public AudioClip startBlockTutorialSound;
-    public AudioClip endBlockTutorialSound;
-    public AudioClip logicBlockTutorialSound;
+    // Audio clips
+    public AudioClip startTutorialClip;
+    public AudioClip startBlockTutorialClip;
+    public AudioClip endBlockTutorialClip;
+    public AudioClip startEndBlockFinishClip;
+    public AudioClip logicBlockTutorialClip;
+    public AudioClip flipSideTutorialClip;
+    public AudioClip finishedTutorialClip;
+
     private new AudioSource audio;
+
+    // Boolean markers to check instruction status
     private bool placedStartBlock = false;
     private bool placedEndBlock = false;
+    private bool placedLogicBlock = false;
+    private bool flippedSide = false;
 
 	// Use this for initialization
 	void Start () {
@@ -24,28 +33,30 @@ public class TutorialManager : MonoBehaviour {
 	}
 
     IEnumerator StartTutorial() {
-        yield return new WaitForSeconds(5);
-        audio.PlayOneShot(startTutorialSound);
-        yield return new WaitForSeconds(startTutorialSound.length + 5);
-        audio.PlayOneShot(startBlockTutorialSound);
-        yield return new WaitForSeconds(startBlockTutorialSound.length + 5);
-        //Invoke("startBlockTutorial", startTutorialSound.length);
-    }
+        // Initial wait for tutorial to start
+        yield return new WaitForSeconds(2);
 
-   /* void startBlockTutorial()
-    {
-        audio.PlayOneShot(startBlockTutorialSound);
-    }*/
+        // Play beginning of tutorial
+        audio.PlayOneShot(startTutorialClip);
+        yield return new WaitForSeconds(startTutorialClip.length + 2);
+
+        // Play second part of tutorial
+        audio.PlayOneShot(startBlockTutorialClip);
+        yield return new WaitForSeconds(startBlockTutorialClip.length + 2);
+
+        // Play third part of tutorial
+        //Invoke("startBlockTutorial", startTutorialSound.length);
+        //yield return new WaitForSeconds(startBlockTutorialClip.length + 3);
+    }
 
     void PlacedStartBlock()
     {
         if(placedStartBlock == false)
         {
-            /* Play End Block tutorial */
-            audio.PlayOneShot(endBlockTutorialSound);
-
-
             placedStartBlock = true;
+
+            /* Play End Block tutorial */
+            audio.PlayOneShot(endBlockTutorialClip);
         }
     }
 
@@ -53,11 +64,45 @@ public class TutorialManager : MonoBehaviour {
     {
         if (placedEndBlock == false)
         {
-            /* Play Logic Block tutorial */
-            audio.PlayOneShot(logicBlockTutorialSound);
-
-            Debug.Log("Placed the end block");
             placedEndBlock = true;
+
+            /* Play Logic Block tutorial */
+            StartCoroutine(startEndBlockFinished());
+            
+            //audio.PlayOneShot(logicBlockTutorialClip);
+            
         }   
+    }
+
+    IEnumerator startEndBlockFinished()
+    {
+        audio.PlayOneShot(logicBlockTutorialClip);
+        yield return new WaitForSeconds(logicBlockTutorialClip.length);
+        audio.PlayOneShot(startEndBlockFinishClip);
+        yield return new WaitForSeconds(startEndBlockFinishClip.length);
+    }
+
+    void PlacedLogicBlock()
+    {
+        if (placedLogicBlock == false)
+        {
+            placedLogicBlock = true;
+
+            /* Play Flip side tutorial */
+            audio.PlayOneShot(flipSideTutorialClip);
+
+        }
+    }
+
+    void FlippedSide()
+    {
+        if (flippedSide == false)
+        {
+            flippedSide = true;
+
+            /* Play finished tutorial clip */
+            audio.PlayOneShot(finishedTutorialClip);
+
+        }
     }
 }
