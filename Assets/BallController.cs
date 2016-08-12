caae1ece0147a852634090e8919524f3345c1d0a
Jesse <jvanderdoes@gmail.com>
2016-08-12 11:58:41 -0700

    Added ball hit and block create sounds

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -1,17 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BallController : MonoBehaviour {
+public class BallController : MonoBehaviour
+{
 
     public GameObject block;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        AudioSource hitSound = GetComponent<AudioSource>();
+        hitSound.volume = hitSound.volume * collision.relativeVelocity.magnitude * 2;
+        hitSound.Play();
+    }
 }
